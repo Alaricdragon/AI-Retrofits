87a334c5b81b9e118d345140f2eda1aa7ac3b357
Alaricdragon <Alaric.meservy@gmail.com>
2021-07-12 15:03:10 -0800

    Revert "Add files via upload"
    This reverts commit 529920dda139957745bccb98861d72859aa09864.

--- jars/src/data/scripts/VIC_ModPlugin.java
+++
@@ -1,9 +0,0 @@
-package data.scripts;
-import com.fs.starfarer.api.BaseModPlugin;
-import com.fs.starfarer.api.Global;
-public class VIC_ModPlugin extends BaseModPlugin {
-    /*@Override
-    public void onApplicationLoad() {
-        throw new ClassCastException("Hello!");
-    }*/
-}
